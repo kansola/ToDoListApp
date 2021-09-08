aa497daca234e0257c7577acd839855fccde60bf
OYINKANSOLA AREMU <adetolaaremu99@gmail.com>
2021-09-08 23:11:24 +0100

    commit

--- Startup.cs
+++ Startup.cs
@@ -87,7 +87,7 @@ namespace ToDoListApp
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
             app.UseAuthentication();
